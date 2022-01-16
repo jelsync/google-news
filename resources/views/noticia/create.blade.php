bd9effc50ff9f59000c7a89b19116ce495b732c7
jelsync <jelsyn@gmail.com>
2022-01-15 23:21:15 -0600

    Estructura base del formulario crear noticia

--- resources/views/noticia/create.blade.php
+++ resources/views/noticia/create.blade.php
@@ -1,1 +1,15 @@
 Crear noticia
+
+<form action="" method="post" enctype="multipart/form-data">
+
+    <label for="TituloNoticia">Titulo de la Noticia</label>
+    <input type="text" name="TituloNoticia">
+    <br>
+    <label for="Descripcion">Descripcion</label>
+    <input type="text" name="Descripcion">
+    <br>
+    <label for="Enlace">Enlace</label>
+    <input type="text" name="Enlace">
+    <br>
+    <input type="submit" name="Crear">
+</form>
